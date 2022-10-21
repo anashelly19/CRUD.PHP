69db99034f9ff0a4c07a464a3f4ae42af5071b78
anashelly19 <anashellypires57@gmail.com>
2022-10-20 21:03:47 -0300

    adicionando arquivo delete.php

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+include "config.php;"
+
+if(isset($_GET['id'])){
+    $id =$_GET['id'];
+    $sql = "DELETE FROM `cliente`.`usuarios` WHERE ´id`= '$id' ";
+
+    $result = $conn -> query($sql);
+    if ($result ==TRUE){
+        echo "Registro deletado com sucesso";
+    }else {
+        echo "ERRO:" .$sql. "<br>" . $conn->error;
+    }
+}
+
+
+
+
+
+?>
